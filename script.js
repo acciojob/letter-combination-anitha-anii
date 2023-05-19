cdb926ce3db7d9c2488562680d8750152d70ce54
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-19 17:46:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+  if (digits.length === 0) {
+    return [];
+  }
+
+  const digitToLetters = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+  };
+
+  const combinations = [];
+
+  backtrack("", digits);
+
+  return combinations;
+
+  function backtrack(currentCombination, remainingDigits) {
+    if (remainingDigits.length === 0) {
+      combinations.push(currentCombination);
+      return;
+    }
+
+    const currentDigit = remainingDigits[0];
+    const letters = digitToLetters[currentDigit];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(currentCombination + letter, remainingDigits.slice(1));
+    }
+  }
 }
 
-module.exports = letterCombinations;
+const input = "23";
+const combinations = letterCombinations(input);
+console.log(combinations);
+
+
+ 
